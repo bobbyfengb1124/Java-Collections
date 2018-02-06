e8bec527dafc207a65065d9b0cd56b5fdcf2a93b
Feng Bo <bobbyfeng@cse.com.sg>
2018-02-06 14:48:58 +0800

    Collections swap, shuffle, min, max, copy and reverse functions

--- src/studio/bo/feng/Main.java
+++ src/studio/bo/feng/Main.java
@@ -3,6 +3,13 @@
  */
 package studio.bo.feng;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import javax.xml.transform.Templates;
+
 /**
  * @author bobbyfeng
  *
@@ -15,19 +22,50 @@ public class Main {
 	public static void main(String[] args) {
 
 		Theatre theatre = new Theatre("Olympian", 8, 12);
-//		theatre.getSeats();
-		if(theatre.reserveSeat("H11")) {
-			System.out.println("Please pay");
+		List<Seat> seatCopy = new ArrayList<>(theatre.seats);
+		printList(seatCopy);
+
+		seatCopy.get(1).reserve();
+		if (theatre.reserveSeat("A02")) {
+			System.out.println("Please pay for A02");
+		} else {
+			System.out.println("Seat already reserved");
 		}
-		else {
-			System.out.println("Sorry, seat is taken");
-		}
+
+		Collections.shuffle(seatCopy);
+		System.out.println("Printing seatCopy");
+		printList(seatCopy);
+		System.out.println("Print theatre.seat");
+		printList(theatre.seats);
+
+		Seat minSeat = Collections.min(seatCopy);
+		Seat maxSeat = Collections.max(seatCopy);
+		System.out.println("Min seat number is " + minSeat.getSeatNumber());
+		System.out.println("Max seat number is " + maxSeat.getSeatNumber());
 		
-		if(theatre.reserveSeat("H11")) {
-			System.out.println("Please pay");
+		sortList(seatCopy);
+		System.out.println("Printing sorted seatCopy");
+		printList(seatCopy);
+		
+		List<Seat> newList = new ArrayList<>(theatre.seats.size());
+		Collections.copy(newList, theatre.seats);
+	}
+
+	public static void printList(List<Seat> list) {
+		for (Seat seat : list) {
+			System.out.print(" " + seat.getSeatNumber());
 		}
-		else {
-			System.out.println("Sorry, seat is taken");
+		System.out.println();
+		System.out.println("=============================================");
+	}
+	
+	public static void sortList(List<? extends Seat> list) {
+		for(int i =0; i<list.size()-1;i++) {
+			for(int j =i+1; j<list.size();j++) {
+				if(list.get(i).compareTo(list.get(j))>0) {
+					Collections.swap(list, i, j);
+				}
+			}
 		}
 	}
 
--- src/studio/bo/feng/Theatre.java
+++ src/studio/bo/feng/Theatre.java
@@ -18,7 +18,7 @@ import java.util.List;
 public class Theatre {
 
 	private final String theatreName;
-	private List<Seat> seats = new ArrayList<>();
+	public List<Seat> seats = new ArrayList<>();
 	
 	
 	public Theatre(String theatreName, int numRows, int seatsPerRow) {
