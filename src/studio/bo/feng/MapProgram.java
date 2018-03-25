3930e53ad27beb328c3bd843e8b6f1b09300713d
bofeng@u.nus.edu <bobbyfeng@cse.com.sg>
2018-03-25 11:54:25 +0800

    added Map replace and start off Adventure Game

--- src/studio/bo/feng/MapProgram.java
+++ src/studio/bo/feng/MapProgram.java
@@ -6,6 +6,8 @@ package studio.bo.feng;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.sound.midi.Soundbank;
+
 /**
  * @author bobbyfeng
  *
@@ -29,7 +31,7 @@ public class MapProgram {
 		languages.put("Python", "an interpreted, object-oriented, high-level programming language with dynamic semantics");
 		languages.put("Algol", "an algorithmic language");
 		System.out.println(languages.put("BASIC", "beginners all purposes symbolic instruction code"));
-		System.out.println(languages.put("Lisp", "therein lies madness"));
+		System.out.println(languages.put("Lisp", "Therein lies madness"));
 		
 		if(languages.containsKey("Java")) {
 			System.out.println("Java is already in the map");
@@ -40,6 +42,21 @@ public class MapProgram {
 		
 		System.out.println("==================================");
 		
+//		languages.remove("Lisp");
+		if(languages.remove("Algol", "an algorithmic language")) {
+			System.out.println("Algol removed.");
+		}else {
+			System.out.println("Algol not removed, key/value pair not found");
+		}
+		
+//		System.out.println(languages.replace("Lisp", "a funtional language with imperative features"));
+		if(languages.replace("Lisp", "Therein lies madness", "a funtional language with imperative features")) {
+			System.out.println("Lisp replaced");
+		}
+		else {
+			System.out.println("Lisp was not replaced");
+		}
+//		System.out.println(languages.replace("Scala", "This will not be replaced"));
 		for(String key : languages.keySet()) {
 			System.out.println(key + " : " + languages.get(key));
 		}
