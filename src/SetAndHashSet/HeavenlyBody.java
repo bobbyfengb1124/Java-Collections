c15141b5ddd405c7dff331db92f9ed9e438bb488
bofeng@u.nus.edu <bobbyfeng@cse.com.sg>
2018-04-23 17:22:30 +0800

    part one of equals and HashCode

--- src/SetAndHashSet/HeavenlyBody.java
+++ src/SetAndHashSet/HeavenlyBody.java
@@ -38,6 +38,21 @@ public final class HeavenlyBody {
 		return new HashSet<>(this.satellites);
 	}
 	
-	
+	@Override
+	public boolean equals(Object obj) {
+		if(this == obj) {
+			return true;
+		}
+		
+		System.out.println("obj.getClass() is " + obj.getClass());
+		System.out.println("this.getClass() is " + this.getClass());
+		
+		if((obj == null) || (obj.getClass() != this.getClass())) {
+			return false;
+		}
+
+		String objName = ((HeavenlyBody) obj).getName();
+		return this.name.equals(objName);
+	}
 
 }
--- src/SetAndHashSet/Main.java
+++ src/SetAndHashSet/Main.java
@@ -105,7 +105,16 @@ public class Main {
         for(HeavenlyBody moon : moons) {
             System.out.println("\t" + moon.getName());
         }
+        
+        HeavenlyBody pluto = new HeavenlyBody("Pluto", 842);
+        planets.add(pluto);
+        
+        for (HeavenlyBody planet : planets) {
+        	System.out.println(planet.getName() + ": " + planet.getOrbitalPeriod());
+        }
 		
+        pluto.equals(pluto);
+        
 	}
 
 }
