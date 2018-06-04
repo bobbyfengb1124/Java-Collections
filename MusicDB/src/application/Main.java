483fbf61c35aff6cb533319fe3d0279b8827fea6
bofeng@u.nus.edu <bobbyfeng@cse.com.sg>
2018-06-04 10:40:26 +0800

    added MusicDB JavaFX project

---
+++ MusicDB/src/application/Main.java
@@ -0,0 +1,39 @@
+package application;
+	
+import application.model.Datasource;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+public class Main extends Application {
+	@Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("Music Database");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+
+    @Override
+    public void init() throws Exception {
+        super.init();
+        if(!Datasource.getInstance().open()) {
+            System.out.println("FATAL ERROR: Couldn't connect to database");
+            Platform.exit();
+        }
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+        Datasource.getInstance().close();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
