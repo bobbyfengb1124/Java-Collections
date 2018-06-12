e44cef468f0c8bc078ccd91f79550a7f65c3c543
bofeng@u.nus.edu <bobbyfeng@cse.com.sg>
2018-06-12 14:03:58 +0800

    added HTTPConnection and Debugger demo

---
+++ DebuggerDemo/src/debugger/StringUtilities.java
@@ -0,0 +1,13 @@
+package debugger;
+
+public class StringUtilities {
+
+	private StringBuilder sBuilder = new StringBuilder();
+    private int charsAdded = 0;
+
+    public void addChar(StringBuilder sBuilder, char c) {
+        this.sBuilder.append(c);
+        charsAdded++;
+    }
+
+}
--- Networking/src/high/level/API/Main.java
+++ Networking/src/high/level/API/Main.java
@@ -3,6 +3,7 @@ package high.level.API;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -12,20 +13,40 @@ public class Main {
 	public static void main(String[] args) {
 
 		try {
-			URL url = new URL("http://example.org");
-			URLConnection urlConnection = url.openConnection();
-            urlConnection.setDoOutput(true);
-            urlConnection.connect();
+			URL url = new URL("https://api.flickr.com/services/feeds/photos_public.gne?tags=dogs");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("User-Agent", "Chrome");
+            connection.setReadTimeout(30000);
+            
+            int responseCode = connection.getResponseCode();
+            System.out.println("Response code: " + responseCode);
+            
+            if(responseCode != 200) {
+                System.out.println("Error reading web page");
+                System.out.println();
+                return;
+            }
 			
-			BufferedReader inputStream = new BufferedReader(
-                    new InputStreamReader(urlConnection.getInputStream()));
+			BufferedReader inputReader = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
 			
-			String line = "";
-            while(line != null) {
-                line = inputStream.readLine();
+			String line;
+
+            while((line = inputReader.readLine()) != null) {
                 System.out.println(line);
             }
-            inputStream.close();
+			
+//			Map<String, List<String>> headerFields = connection.getHeaderFields();
+//          for(Map.Entry<String, List<String>> entry : headerFields.entrySet()) {
+//              String key = entry.getKey();
+//              List<String> value = entry.getValue();
+//              System.out.println("-----key = " + key);
+//              for(String string: value) {
+//                  System.out.println("value = " + value);
+//              }
+//          }
+            inputReader.close();
 		} catch (MalformedURLException e) {
 			System.out.println("URL Malformed: " + e.getMessage());
 		} catch(IOException e) {
