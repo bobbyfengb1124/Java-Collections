0fca428dfad186413f7feabc936a51ca694f2494
bofeng@u.nus.edu <bobbyfeng@cse.com.sg>
2018-06-14 15:43:01 +0800

    added debugger demo 2

--- DebuggerDemo/src/debugger/StringUtilities.java
+++ DebuggerDemo/src/debugger/StringUtilities.java
@@ -6,8 +6,17 @@ public class StringUtilities {
     private int charsAdded = 0;
 
     public void addChar(StringBuilder sBuilder, char c) {
-        this.sBuilder.append(c);
+        sBuilder.append(c);
         charsAdded++;
+    }
+    
+    public String upperAndPrefix(String str) {
+        String upper = str.toUpperCase();
+        return "Prefix_" + upper;
+    }
+
+    public String addSuffix(String str) {
+        return str + "__Suffix";
     }
 
 }
