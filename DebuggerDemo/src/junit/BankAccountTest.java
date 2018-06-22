3289b7c9b6185259076af18437e9f0c74cc0a661
bofeng@u.nus.edu <bobbyfeng@cse.com.sg>
2018-06-22 17:45:55 +0800

    added junit demo

---
+++ DebuggerDemo/src/junit/BankAccountTest.java
@@ -0,0 +1,45 @@
+package junit;
+
+import static junit.framework.TestCase.fail;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Created by timbuchalka on 20/11/16.
+ */
+public class BankAccountTest {
+	@Test
+	public void deposit() throws Exception {
+		BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
+		double balance = account.deposit(200.00, true);
+		assertEquals(1200.00, balance, 0);
+	}
+
+	@Test
+	public void withdraw() throws Exception {
+		fail("This test has yet to be implemented");
+	}
+
+	@Test
+	public void getBalance_deposit() throws Exception {
+		BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
+		account.deposit(200.00, true);
+		assertEquals(1200.00, account.getBalance(), 0);
+	}
+
+	@Test
+	public void getBalance_withdraw() throws Exception {
+		BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
+		account.withdraw(200.00, true);
+		assertEquals(800.00, account.getBalance(), 0);
+	}
+
+	@Test
+	public void isChecking_true() {
+		BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
+		assertTrue("The account is NOT a checking account", account.isChecking());
+
+	}
+}
---
+++ DebuggerDemo/src/junit/Main.java
@@ -0,0 +1,8 @@
+package junit;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("The application is running");
+    }
+}
