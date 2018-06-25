1f2fb92bd59e17a290f6ce84bb1140c66eed597c
bofeng@u.nus.edu <bobbyfeng@cse.com.sg>
2018-06-25 09:21:09 +0800

    added junit parameterized test

--- DebuggerDemo/src/junit/BankAccount.java
+++ DebuggerDemo/src/junit/BankAccount.java
@@ -33,6 +33,10 @@ public class BankAccount {
     // at a branch, with a teller.
     // It's false if the customer is performing the transaction at an ATM
     public double withdraw(double amount, boolean branch) {
+    	if ((amount > 500.00) & !branch) {
+            throw new IllegalArgumentException();
+        }
+    	
         balance -= amount;
         return balance;
     }
--- DebuggerDemo/src/junit/BankAccountTest.java
+++ DebuggerDemo/src/junit/BankAccountTest.java
@@ -4,42 +4,73 @@ import static junit.framework.TestCase.fail;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
  * Created by timbuchalka on 20/11/16.
  */
 public class BankAccountTest {
+	
+	private BankAccount account;
+	private static int count;
+	
+	@BeforeClass
+    public static void beforeClass() {
+        System.out.println("This executes before any test cases. Count = " + count++);
+    }
+	
+	@Before
+    public void setup() {
+		account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
+        System.out.println("Running a test...");
+    }
+	
 	@Test
 	public void deposit() throws Exception {
-		BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
 		double balance = account.deposit(200.00, true);
 		assertEquals(1200.00, balance, 0);
 	}
 
 	@Test
-	public void withdraw() throws Exception {
-		fail("This test has yet to be implemented");
-	}
+    public void withdraw_branch() throws Exception {
+        double balance = account.withdraw(600.00, true);
+        assertEquals(400.00, balance, 0);
+    }
+
+	@org.junit.Test(expected = IllegalArgumentException.class)
+    public void withdraw_notBranch() throws Exception {
+       account.withdraw(600.00, false);
+       fail("Should have thrown an IllegalArgumentException");
+    }
 
 	@Test
 	public void getBalance_deposit() throws Exception {
-		BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
 		account.deposit(200.00, true);
 		assertEquals(1200.00, account.getBalance(), 0);
 	}
 
 	@Test
 	public void getBalance_withdraw() throws Exception {
-		BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
 		account.withdraw(200.00, true);
 		assertEquals(800.00, account.getBalance(), 0);
 	}
 
 	@Test
 	public void isChecking_true() {
-		BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
 		assertTrue("The account is NOT a checking account", account.isChecking());
-
 	}
+	
+	@AfterClass
+    public static void afterClass() {
+        System.out.println("This executes after any test cases. Count = " + count++);
+    }
+	
+	@After
+    public void teardown() {
+        System.out.println("Count = " + count++);
+    }
 }
