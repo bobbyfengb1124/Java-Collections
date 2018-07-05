cbb6fedcb64b96b5d26bb38b189a110a3bda1d0d
bofeng@u.nus.edu <bobbyfeng@cse.com.sg>
2018-07-05 15:00:05 +0800

    Signed-off-by: bofeng@u.nus.edu <bobbyfeng@cse.com.sg>

--- DebuggerDemo/src/input/output/Locations.java
+++ DebuggerDemo/src/input/output/Locations.java
@@ -20,6 +20,7 @@ public class Locations implements Map<Integer, Location> {
 		try (FileWriter locFile = new FileWriter("locations.txt");
 				FileWriter dirFile = new FileWriter("directions.txt")) {
 			for (Location location : locations.values()) {
+				
 				locFile.write(location.getLocationID() + "," + location.getDescription() + "\r\n");
 				for (String direction : location.getExits().keySet()) {
 					dirFile.write(location.getLocationID() + "," + direction + "," + location.getExits().get(direction)
