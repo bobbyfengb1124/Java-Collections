838c5552e2b6ddb675ce47a0882cc49cd6506475
bofeng@u.nus.edu <bobbyfeng@cse.com.sg>
2018-08-29 16:43:49 +0800

    added auto boxing challenge

---
+++ DebuggerDemo/src/autoboxing/challenge/Customer.java
@@ -0,0 +1,29 @@
+package autoboxing.challenge;
+
+import java.util.ArrayList;
+
+public class Customer {
+	
+	private String name;
+	private ArrayList<Double> transactions;
+	
+	public Customer(String name, double initialAmount) {
+		this.name = name;
+		this.transactions = new ArrayList<Double>();
+        addTransaction(initialAmount);
+	}
+	
+	public void addTransaction(double amount) {
+
+		transactions.add(amount);
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public ArrayList<Double> getTransactions() {
+        return transactions;
+    }
+	
+}
