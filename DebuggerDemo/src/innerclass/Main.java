53400dfbabaff846e3afa5e7d2deb43872bb6a61
bofeng@u.nus.edu <bobbyfeng@cse.com.sg>
2018-09-15 21:37:51 +0800

    added inner class

---
+++ DebuggerDemo/src/innerclass/Main.java
@@ -0,0 +1,14 @@
+package innerclass;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Gearbox mcLaren = new Gearbox(6);
+//		Gearbox.Gear first = mcLaren.new Gear(1, 12.3);
+//		Gearbox.Gear second = new mcLaren.Gear(1, 12.3);
+//		Gearbox.Gear third = new Gearbox.Gear(1, 12.3);
+//		System.out.println(first.driveSpeed(1000));
+	}
+
+}
