54e155c07725f08e5456f322f1a450da779f9ea2
bofeng@u.nus.edu <bofeng@u.nus.edu>
2018-10-24 15:48:55 +0800

    added BetterGraph solution

--- DebuggerDemo/src/ds/graph/BetterGraph.java
+++ DebuggerDemo/src/ds/graph/BetterGraph.java
@@ -1,24 +1,87 @@
 package ds.graph;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.sun.prism.impl.VertexBuffer;
+
+import ds.singlylinkedlist.SinglyLinkedList;
+
 public class BetterGraph {
 
-	public BetterGraph(int vCount, String directed) {
-		// TODO Auto-generated constructor stub
+	Vertex[] arrayOfLists;
+	int indexCounter = 0;
+	boolean undirected = true;
+
+	class Node {
+		public int vertexIdx;
+		public Node next;
+
+		public Node(int vertexIdx, Node node) {
+			this.vertexIdx = vertexIdx;
+			next = node;
+		}
 	}
 
-	public void addVertex(String string) {
-		// TODO Auto-generated method stub
-		
+	class Vertex {
+		String name;
+		Node adjList;
+
+		Vertex(String name, Node aNode) {
+			this.name = name;
+			this.adjList = aNode;
+		}
 	}
 
-	public void addEdge(String string, String string2) {
-		// TODO Auto-generated method stub
-		
+	public BetterGraph(int vCount, String graphType) {
+		if (graphType.equals("directed")) {
+			undirected = false;
+		}
+
+		arrayOfLists = new Vertex[vCount];
+	}
+
+	public void addVertex(String vertexName) {
+		// ArrayList<String> value = new ArrayList<>();
+		// adjacents.put(vertex, value);
+
+		arrayOfLists[indexCounter] = new Vertex(vertexName, null);
+		indexCounter++;
+	}
+
+	public void addEdge(String srcVertexName, String destVertexName) {
+		// ArrayList<String> value = adjacents.get(vertex);
+		// if(value!=null) {
+		// value.add(edge);
+		// }
+
+		int v1Idx = indexForName(srcVertexName);
+		int v2Idx = indexForName(destVertexName);
+		arrayOfLists[v1Idx].adjList = new Node(v2Idx, arrayOfLists[v1Idx].adjList);
+		if (undirected) {
+			arrayOfLists[v2Idx].adjList = new Node(v1Idx, arrayOfLists[v2Idx].adjList);
+		}
+	}
+
+	private int indexForName(String name) {
+		for (int v = 0; v < arrayOfLists.length; v++) {
+			if (arrayOfLists[v].name.equals(name)) {
+				return v;
+			}
+		}
+		return -1;
 	}
 
 	public void print() {
-		// TODO Auto-generated method stub
-		
+
+		System.out.println();
+		for(int v=0;v<arrayOfLists.length;v++) {
+			System.out.print(arrayOfLists[v].name);
+			for(Node aNode = arrayOfLists[v].adjList; aNode!=null; aNode = aNode.next) {
+				System.out.print(" --> " + arrayOfLists[aNode.vertexIdx].name);
+			}
+			System.out.println();
+		}
 	}
 
 }
