bdbad321eccdf25232db2315805eebc5befa28ce
bofeng@u.nus.edu <bobbyfeng@cse.com.sg>
2019-02-27 17:00:10 +0800

    added LeetCode Challenge 287 Find Duplicates

---
+++ DebuggerDemo/src/leetcode/challenge/FindDuplicate.java
@@ -0,0 +1,31 @@
+package leetcode.challenge;
+
+public class FindDuplicate {
+
+	public static void main(String[] args) {
+		int[] a1 = new int[] { 1, 3, 4, 2, 2 };
+		System.out.println(findDuplicate(a1));
+
+		int[] a2 = new int[] { 3, 1, 3, 4, 2 };
+		System.out.println(findDuplicate(a2));
+	}
+
+	public static int findDuplicate(int[] nums) {
+		int slow = nums[0];
+		int fast = nums[0];
+
+		do {
+			slow = nums[slow];
+			fast = nums[nums[fast]];
+		}while (slow != fast);
+		
+		fast = nums[0];
+		while(fast !=slow) {
+			fast = nums[fast];
+			slow = nums[slow];
+		}
+
+		return fast;
+	}
+
+}
