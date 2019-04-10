8b6d83fff84f8648ca4f2fa1afd50d7534d3f63c
bofeng@u.nus.edu <bobbyfeng@cse.com.sg>
2019-04-10 16:16:39 +0800

    added leetcode challenge 135 candy - greedy algorithm.

---
+++ DebuggerDemo/src/leetcode/challenge/Candy.java
@@ -0,0 +1,41 @@
+package leetcode.challenge;
+
+import java.util.Arrays;
+
+
+public class Candy {
+
+	public static void main(String[] args) {
+		System.out.println(candy(new int[] { 1, 0, 2 }));
+
+		System.out.println(candy(new int[] { 1, 2, 2 }));
+
+		System.out.println(candy(new int[] { 1, 3, 2, 2, 1 }));
+		System.out.println(candy(new int[] { 1, 2, 87, 87, 87, 2, 1 }));
+	}
+
+	public static int candy(int[] ratings) {
+		int total = 0;
+		int[] tmp = new int[ratings.length];
+		Arrays.fill(tmp, 1);
+
+		for (int i = 1; i < ratings.length; i++) {
+			if (ratings[i] > ratings[i - 1]) {
+				tmp[i] = tmp[i - 1] + 1;
+			}
+		}
+
+		for (int i = ratings.length - 2; i >= 0; i--) {
+			if (ratings[i] > ratings[i + 1] && tmp[i] < tmp[i + 1] + 1) {
+				tmp[i] = tmp[i + 1] + 1;
+			}
+		}
+
+		for (int i = 0; i < ratings.length; i++) {
+			total = total + tmp[i];
+		}
+
+		return total;
+	}
+
+}
